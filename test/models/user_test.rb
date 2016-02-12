9f4f3b2e87cbe76fec46bb3615eeb4e469fe0f38
Matt Velez <mzvelez75@gmail.com>
2016-02-12 14:50:53 +0000

    Add password security

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "mariali", password_confirmation: "mariali")
   end
   
   test "should be valid" do
