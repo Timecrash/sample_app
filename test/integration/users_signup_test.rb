942afb4a749ba1fbedb4d85a3cf3c4b2acca4977
Matt Velez <mzvelez75@gmail.com>
2016-02-17 19:46:33 +0000

    Finished exercises

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -10,6 +10,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                password_confirmation: 'bar' }
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanantion'
+    assert_select 'div.alert'
   end
   
   test "valid signup information" do
@@ -21,5 +23,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password_confirmation: "password" }
     end
     assert_template 'users/show'
+    assert_not flash.empty?
+    assert_select 'div.alert'
   end
 end
