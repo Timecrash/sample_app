5dbb0e2236f3502752704adf15396c468f31d52b
Matt Velez <mzvelez75@gmail.com>
2016-02-17 19:16:56 +0000

    Add flash for successful user generation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Welcome to the Sample App, #{@user.name}!"
       redirect_to @user
     else
       render 'new'
