e95583310177dc60dabdfb29faefd55c80a7f540
Matt Velez <mzvelez75@gmail.com>
2016-02-22 16:27:20 +0000

    Finish integration test for login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       #Log user in
     else
-      flash[:danger] = 'Invalid email/password combination'
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
