0f47158d0763c70b66bfed6ed5a4d09df58fa9a5
Matt Velez <mzvelez75@gmail.com>
2016-02-22 16:20:57 +0000

    Add integration test for login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,13 @@ class SessionsController < ApplicationController
   end
   
   def create
-    render 'new'
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      #Log user in
+    else
+      flash[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
   end
   
   def destroy
