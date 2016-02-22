d73eea2a1ad38b61fe123322f0285375d865c26e
Matt Velez <mzvelez75@gmail.com>
2016-02-22 19:56:51 +0000

    Create test for valid login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in(@user)
       flash[:success] = "Welcome to the Sample App, #{@user.name}!"
       redirect_to @user
     else
