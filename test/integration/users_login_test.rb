dfea103f222d4b34b1dc94aebc69ed100485c8d2
Matt Velez <mzvelez75@gmail.com>
2016-02-23 17:54:06 +0000

    Generate SessionsHelper test

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -47,4 +47,14 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_template 'users/show'
     assert is_logged_in?
   end
+  
+  test 'login with remembering' do
+    log_in_as(@user, remember_me: '1')
+    assert_not_nil cookies['remember_token']
+  end
+  
+  test 'login without remembering' do
+    log_in_as(@user, remember_me: '0')
+    assert_nil cookies['remember_token']
+  end
 end
