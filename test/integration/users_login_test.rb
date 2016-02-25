9bcc549ddb5a4c699263c4c52395f6bcdde2c23a
Matt Velez <mzvelez75@gmail.com>
2016-02-25 21:26:51 +0000

    Add integration test for User editing

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
   def setup
-    @user = users(:michael)
+    @user = users(:matt)
   end
   
   test 'login with invalid information' do
