c7477d9bd4f46d6dea64bf1fd0742cc62272d138
Matt Velez <mzvelez75@gmail.com>
2016-03-09 21:20:35 +0000

    Prepare for adding SendGrid

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -12,7 +12,9 @@ class UserMailerPreview < ActionMailer::Preview
   # Preview this email at
   # http://rails-tutorial-timecrash1.c9users.io/rails/mailers/user_mailer/password_reset
   def password_reset
-    UserMailer.password_reset
+    user = User.first
+    user.reset_token = User.new_token
+    UserMailer.password_reset(user)
   end
 
 end
--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -13,4 +13,15 @@ class UserMailerTest < ActionMailer::TestCase
     assert_match user.activation_token,   mail.body.encoded
     assert_match CGI::escape(user.email), mail.body.encoded
   end
+  
+  test "password_reset" do
+    user = users(:matt)
+    user.reset_token = User.new_token
+    mail = UserMailer.password_reset(user)
+    assert_equal "Password Reset", mail.subject
+    assert_equal [user.email], mail.to
+    assert_equal ["noreply@example.com"], mail.from
+    assert_match user.reset_token,        mail.body.encoded
+    assert_match CGI::escape(user.email), mail.body.encoded
+  end
 end
