20bcd9270e640f3df4f970798bb46f716de59508
Matt Velez <mzvelez75@gmail.com>
2016-03-09 18:57:17 +0000

    Finish integration test for email activation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user && @user.authenticate(params[:session][:password])
-      if user.activated?
+      if @user.activated?
         log_in @user
         params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
         flash[:success] = "Welcome back, #{@user.name}!"
