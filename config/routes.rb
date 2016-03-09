476a82ea8b058b6a69ce75e1376faf7dad222420
Matt Velez <mzvelez75@gmail.com>
2016-03-09 14:40:45 +0000

    Add activation to User model

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   post    'login'   => 'sessions#create'
   delete  'logout'  => 'sessions#destroy'
   resources :users
+  resources :account_activations, only: [:edit]
 end
