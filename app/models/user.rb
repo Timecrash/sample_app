a574bbb54e274462825abfbbc248ad68bf36eec8
Matt Velez <mzvelez75@gmail.com>
2016-03-17 00:40:15 +0000

    Add create/delete to microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -70,6 +70,11 @@ class User < ActiveRecord::Base
     reset_sent_at < 2.hours.ago
   end
   
+  #Defines a proto-feed.
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
+  
   private
   #Converts email to all lowercase.
   def downcase_email
